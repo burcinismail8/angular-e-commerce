d26c556ef6297f7e94b2d1228cc1a46d36a26de2
Burchin Ismail <burcinismail8@gmail.com>
2022-06-08 09:44:40 +0300

    initial commit

---
+++ apps/ng-shop/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-e-commerce-app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'ng-shop';
+}
