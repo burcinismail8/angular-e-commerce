0ac9bd3ca4b668a7ccac938d2b513dd9296509c8
Burchin Ismail <burcinismail8@gmail.com>
2022-06-10 09:04:11 +0300

    Added sidebar

--- apps/admin/src/app/app.component.ts
+++ apps/admin/src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-e-commerce-app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'admin';
